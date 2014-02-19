c159eb5f75be83b5195fe108260e596a1e38a646
theengineear <andseier@gmail.com>
2014-02-18 18:25:32 -0800

    Added plotly subpackage to renderers package for PlotlyRenderer obj.
    Plotly subpackage includes the plotly_renderer module which contains the
    PlotlyRenderer object which inherits from the renderers.base.Renderer
    class. It reimplements the suggested methods commented on in
    renderers.base.Renderer.
    
    The plotly_utils module is included to separate basic style and
    translation functionality from the main objective of the plotly_renderer
    module which is to construct the data and layout dictionaries.
    
    The PlotlyRenderer may differ slightly from the functionality of other
    renderers as it just 'updates' its member dictionaries (self.data and
    self.layout) as the Exporter object crawls around the matplotlib figure.
    When the Exporter reaches the end of its crawl and the renderer 'closes'
    the figure, PlotlyRenderer makes a call to plotly with the data and
    layout dictionaries. This causes your browser to open a page pointing to
    your new plotly plot!
    
    It may or may not be of use to keep the renderers separated by
    subpackages as was implemented with the plotly subpackage. TBD.

---
+++ mplexporter/renderers/plotly/plotly_renderer.py
@@ -0,0 +1,78 @@
+"""
+Plotly Renderer
+================
+This is a renderer class to be used with an exporter for rendering plots in Plotly!
+"""
+import plotly
+
+from . import plotly_utils
+from .. base import Renderer
+
+
+class PlotlyRenderer(Renderer):
+    def __init__(self, username=None, api_key=None):
+        self.output = ""
+        self.username = username
+        self.api_key = api_key
+        self.data = []
+        self.layout = {}
+        self.axis_ct = 0
+
+    def open_figure(self, fig, properties):
+        self.output += "opening figure\n"
+        self.layout['width'] = int(properties['figwidth']*properties['dpi'])
+        self.layout['height'] = int(properties['figheight']*properties['dpi'])
+
+    def close_figure(self, fig):
+        self.output += "closing figure\n"
+        print self.output
+        py = plotly.plotly(self.username, self.api_key)
+        py.plot(self.data, layout=self.layout)  # make call to plotly!
+
+    def open_axes(self, ax, properties):
+        self.output += "  opening axes\n"
+        self.axis_ct += 1
+        layout = {}
+        layout['title'] = properties['title']
+        xaxis = {}
+        xaxis['range'] = properties['xlim']
+        xaxis['title'] = properties['xlabel']
+        xaxis['showgrid'] = properties['xgrid']
+        yaxis = {}
+        yaxis['range'] = properties['ylim']
+        yaxis['title'] = properties['ylabel']
+        yaxis['showgrid'] = properties['ygrid']
+        layout['xaxis'] = xaxis
+        layout['yaxis'] = yaxis
+        self.layout = layout
+
+    def close_axes(self, ax):
+        self.output += "  closing axes\n"
+
+    def draw_line(self, data, coordinates, style):
+        self.output += "    draw line with {0} points\n".format(data.shape[0])
+        data_dict = {'x': [], 'y': []}
+        for xy_pair in data:
+            data_dict['x'] += [xy_pair[0]]
+            data_dict['y'] += [xy_pair[1]]
+        data_dict['mode'] = 'lines'
+        data_dict['line'] = {}
+        data_dict['line']['opacity'] = style['alpha']
+        data_dict['line']['width'] = style['linewidth']
+        data_dict['line']['dash'] = plotly_utils.convert_dash(style['dasharray'])
+        self.data += data_dict,
+
+    def draw_markers(self, data, coordinates, style):
+        self.output += "    draw {0} markers\n".format(data.shape[0])
+        data_dict = {'x': [], 'y': []}
+        for xy_pair in data:
+            data_dict['x'] += [xy_pair[0]]
+            data_dict['y'] += [xy_pair[1]]
+        data_dict['mode'] = 'markers'
+        data_dict['marker'] = {}
+        data_dict['marker']['opacity'] = style['alpha']
+        data_dict['marker']['color'] = style['facecolor']
+        # need to incorporate style['edgecolor']
+        data_dict['marker']['symbol'] = plotly_utils.convert_symbol(style['marker'])
+        # not sure whether we need to incorporate style['markerpath']
+        self.data += data_dict,
