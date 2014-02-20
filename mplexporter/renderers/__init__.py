302112a87db10c436bf8974605c7e1fccef7f2d4
theengineear <andseier@gmail.com>
2014-02-19 18:19:04 -0800

    Functional IPython notebook in notebooks directory.
    IPython notebook runs, but plotly plot is not embedded in notebook yet.

--- mplexporter/renderers/__init__.py
+++ mplexporter/renderers/__init__.py
@@ -2,4 +2,4 @@ from .base import Renderer
 from .vega_renderer import VegaRenderer, fig_to_vega
 from .vincent_renderer import VincentRenderer, fig_to_vincent
 from .example_renderer import ExampleRenderer
-from .plotly import PlotlyRenderer
+from .plotly import PlotlyRenderer, fig_to_plotly
