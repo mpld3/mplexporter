a42e690ac65339a1d410b7e24e011a80437471bc
Matthew Wakefield <MattJWakefield@gmail.com>
2016-03-22 13:13:12 +1100

    ensure drawstyle is not reset in draw_line

--- mplexporter/exporter.py
+++ mplexporter/exporter.py
@@ -183,7 +183,8 @@ class Exporter(object):
                                                    ax, line.get_xydata(),
                                                    force_trans=force_trans)
         linestyle = utils.get_line_style(line)
-        if linestyle['dasharray'] is None:
+        if (linestyle['dasharray'] is None
+                and linestyle['drawstyle'] is 'default'):
             linestyle = None
         markerstyle = utils.get_marker_style(line)
         if (markerstyle['marker'] in ['None', 'none', None]
