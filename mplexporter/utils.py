369db99bd1e8a668df1c6e4e64167760c31022e7
David Straub <david.straub@tum.de>
2017-03-11 13:29:07 +0100

    export_color: only convert to rgba if alpha channel is <1

--- mplexporter/utils.py
+++ mplexporter/utils.py
@@ -22,6 +22,9 @@ def export_color(color):
     """Convert matplotlib color code to hex color code"""
     if color is None or colorConverter.to_rgba(color)[3] == 0:
         return 'none'
+    elif colorConverter.to_rgba(color)[3] == 1:
+        rgb = colorConverter.to_rgb(color)
+        return '#{0:02X}{1:02X}{2:02X}'.format(*(int(255 * c) for c in rgb))
     else:
         c = colorConverter.to_rgba(color)
         return "rgba(" + ", ".join(str(int(np.round(val * 255)))
