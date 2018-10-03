4d027e47b040f5d3a3ed6de49ef015ad41c23bfb
Parham Saidi <parham@parha.me>
2018-10-03 17:32:14 +0200

    added matplotlib.ticker.IndexFormatter serializer

--- mplexporter/utils.py
+++ mplexporter/utils.py
@@ -190,6 +190,7 @@ def get_axis_properties(axis):
     props = {}
     label1On = axis._major_tick_kw.get('label1On', True)
 
+    print "here"
     if isinstance(axis, matplotlib.axis.XAxis):
         if label1On:
             props['position'] = "bottom"
@@ -206,17 +207,22 @@ def get_axis_properties(axis):
     # Use tick values if appropriate
     locator = axis.get_major_locator()
     props['nticks'] = len(locator())
-    if isinstance(locator, ticker.FixedLocator):
+    if type(locator) in [ticker.FixedLocator, ticker.AutoLocator]:
         props['tickvalues'] = list(locator())
     else:
         props['tickvalues'] = None
 
     # Find tick formats
+    props['tickformat_formatter'] = ""
     formatter = axis.get_major_formatter()
     if isinstance(formatter, ticker.NullFormatter):
         props['tickformat'] = ""
+    elif isinstance(formatter, ticker.IndexFormatter):
+        props['tickformat'] = [text.get_text() for text in axis.get_ticklabels()]
+        props['tickformat_formatter'] = "index"
     elif isinstance(formatter, ticker.FixedFormatter):
         props['tickformat'] = list(formatter.seq)
+        props['tickformat_formatter'] = "fixed"
     elif not any(label.get_visible() for label in axis.get_ticklabels()):
         props['tickformat'] = ""
     else:
